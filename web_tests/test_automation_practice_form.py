b05061744c7e4cc4e4cb6786f394678efb7eb3ea
krivorucenkoaleksandra <alexsakriv@gmail.com>
2023-02-24 16:25:13 +0300

    добавила выбор из выпадающего списка

--- web_tests/test_automation_practice_form.py
+++ web_tests/test_automation_practice_form.py
@@ -13,16 +13,18 @@ def test_submit():
     browser.element('#userEmail').type('test@test.com')
     browser.element('[name=gender][value=Female]+label').click()
     browser.element('#userNumber').type('8922121245')
+
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click().element('option[value="11"]').click()
     browser.element('.react-datepicker__year-select').click().element('option[value="1997"]').click()
     browser.element('.react-datepicker__week>.react-datepicker__day--011').click()
-    # browser.element('#subjectsContainer').click()
-    # browser.element('#subjectsInput').type('computer')
-    # browser.element('//*[class="subjects-auto-complete__menu"]//div[.//*[.="Computer Science"]]').click()
-    browser.all('[for^=hobbies-checkbox]').element_by(have.exact_text('Sports'))
-    browser.all('[id^=hobbies-checkbox]').element_by(have.exact_text('Reading'))
-    browser.all('[id^=hobbies-checkbox]').element_by(have.exact_text('Music'))
+
+    browser.element('#subjectsInput').click().type('computer')
+    browser.all('.subjects-auto-complete__option').element_by(have.exact_text('Computer Science')).click()
+
+    browser.all('[for^=hobbies-checkbox]').element_by(have.exact_text('Sports')).click()
+    browser.all('[for^=hobbies-checkbox]').element_by(have.exact_text('Reading')).click()
+    browser.all('[for^=hobbies-checkbox]').element_by(have.exact_text('Music')).click()
 
 
     # THEN
